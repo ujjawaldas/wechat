9e9f9da6bf3e53258e19ca09d3ba6bb4470315dd
Ujjawal Kumar Das <ukdas787@gmail.com>
2022-04-16 18:53:04 +0530

    Create chat app

---
+++ public/client.js
@@ -0,0 +1,63 @@
+const socket = io()
+let name;
+let textarea = document.querySelector('#textarea')
+let messageArea = document.querySelector('.message__area')
+let sendBtn = document.querySelector('.fa.fa-send-o')
+
+do{
+   name = prompt('Please enter your name: ')
+}while(!name)
+
+
+
+textarea.addEventListener('keyup', (e) =>{
+    if(e.key === 'Enter') {
+        if (e.target.value !== ' '){
+            sendMessage(e.target.value)
+
+        }
+    }
+})
+
+sendBtn.addEventListener('click', () => {
+    if (textarea.value !== ''){
+        sendMessage(textarea.value)
+    }
+})
+
+function sendMessage(message){
+    let msg = {
+        user: name,
+        message:message.trim()
+    }
+    appendMessage(msg, 'outgoing')
+    textarea.value = ''
+    scrollToBottom()
+    
+    socket.emit('message', msg)
+}
+
+function appendMessage(msg, type){
+    let mainDiv = document.createElement('div')
+    let className = type
+    mainDiv.classList.add(className, 'message')
+
+    let markup = 
+        `<h4>${msg.user}</h4>
+        <p>${msg.message}</p>`
+
+        mainDiv.innerHTML = markup
+        messageArea.appendChild(mainDiv)
+}
+
+
+
+socket.on('message', (msg) =>{
+    appendMessage(msg, 'incoming')
+    scrollToBottom()
+})
+
+function scrollToBottom(){
+    messageArea.scrollTop = messageArea.scrollHeight
+}
+
